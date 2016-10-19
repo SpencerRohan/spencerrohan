1ecba9bb3aaed9ce7e3edb5d85ed6d381d032a4d
Spencer Rohan <spencermrohan@gmail.com>
2016-10-19 18:47:00 -0500

    Font & Skeleton Complete

--- index.php
+++ index.php
@@ -8,25 +8,10 @@
 	</head>
 	<body>
 		<header>
+			<div class="logo-container">
+				<h1 class="logo">Spencer Rohan</h1>
+			</div>
 		</header>
-		
-		<div class="row">
-		  <div class="col-sm-6">
-		    <div class="card card-block">
-		      <h3 class="card-title">Special title treatment</h3>
-		      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-		      <a href="#" class="btn btn-primary">Go somewhere</a>
-		    </div>
-		  </div>
-		  <div class="col-sm-6">
-		    <div class="card card-block">
-		      <h3 class="card-title">Special title treatment</h3>
-		      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-		      <a href="#" class="btn btn-primary">Go somewhere</a>
-		    </div>
-		  </div>
-		</div>		
-
 
 		<footer>
 	  </footer>
