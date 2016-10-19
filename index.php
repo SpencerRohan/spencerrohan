a969deaf32cf15bbb5ca5da03a2a7c179700a3db
Spencer Rohan <spencermrohan@gmail.com>
2016-10-19 14:35:27 -0500

    ADD SEO

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Spencer M. Rohan</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="assets/css/app.css">	
+		<?php include 'includes/seo.php' ?>
 	</head>
 	<body>
 		<header>
@@ -12,9 +12,6 @@
 
 		<footer>
 	  </footer>
-<!-- 		<script type="text/javascript" src="src/vendor/jquery/dist/jquery.js" ></script>
-		<script type="text/javascript" src="src/vendor/materialize/dist/js/materialize.js" ></script>
-		<script type="text/javascript" src="src/js/app.js" ></script> -->
 		<script type="text/javascript" src="assets/js/app.js"></script>
 	</body>
 </html>
