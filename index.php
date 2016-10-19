99b089c140be337bd6e038d9475c6558e6b78581
Spencer Rohan <spencermrohan@gmail.com>
2016-10-19 13:43:23 -0500

    First Compile

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
-<p>Renovations under way - please check back soon.<br>
--Spencer<br>
-10-17-2016<br></p>
 
-<p>SpencerMRohan@gmail.com<br>
-269-599-3279</p>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Spencer M. Rohan</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="assets/css/app.css">	
+	</head>
+	<body>
+		<header>
+		</header>
+
+		<footer>
+	  </footer>
+<!-- 		<script type="text/javascript" src="src/vendor/jquery/dist/jquery.js" ></script>
+		<script type="text/javascript" src="src/vendor/materialize/dist/js/materialize.js" ></script>
+		<script type="text/javascript" src="src/js/app.js" ></script> -->
+		<script type="text/javascript" src="assets/js/app.js"></script>
+	</body>
+</html>
