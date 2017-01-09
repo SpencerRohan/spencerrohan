8205ff24792a5c793b07f590c6697b0b03e19048
Spencer Rohan <spencermrohan@gmail.com>
2017-01-08 23:47:14 -0600

    UPDATE Modal stylings & FIX Broken links

---
+++ includes/modaldevskills.php
@@ -0,0 +1,20 @@
+<!-- Modal -->
+<div class="modal fade" id="mdevskills" tabindex="-1" role="dialog" aria-labelledby="modal-devskills" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="vid-1-title">Developer Skills</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          <img src="assets/imgs/underconstruction.png" height="130px" width="auto" alt="Under Construction - Coming Soon">
+          <h4 class="text-center filmotypelasalle">Coming Soon!</h4>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-primary btn-sm" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -126,7 +126,7 @@
 										
 									
 									
-										<div class="col-md-4 col-xs-6">
+										<!-- <div class="col-md-4 col-xs-6">
 											<a id="spot-video-1" href="#!spot-video" data-toggle="modal" data-target="#mvid1" data-title="Hollus - \"Rally Cry\" | Music Video"><img src="assets/imgs/covers/rally-cry.jpg" height="75px" width="auto" alt="Hollus Music Video">
 											</a>
 										</div>
@@ -137,7 +137,7 @@
 										<div class="col-md-4 col-xs-6">
 											<a id="spot-video-3" href="#!spot-video" data-toggle="modal" data-target="#mvid3" data-title="Latest Show Ever - Chicago Outfit TV Pilot"><img src="assets/imgs/covers/outfit.jpg" height="75px" width="auto" alt="Latest Show Ever TV Pilot">
 											</a>
-										</div>
+										</div> -->
 										
 									</div>
 									<hr class="clear-both">
@@ -156,8 +156,8 @@
 									<div class="parallax"><img src="<?php echo $url.$image[2] ?>"></div>
 								</div>
 								<section class="content-details">
-									<p>Dev Bootcamp grad, developer with attitude - front, back, & full stack <em>JACK!</em> See the <a href="#!">coding skills</a> that occupy the brain waves. Always working towards new techniques, learning more, and living out of the comfort zone.</p>
-									<p class="text-center"><a href="/assets/docs/SpencerRohanResume.pdf">Resume</a> 	&#x25b0; <a href="https://github.com/spencerrohan" target="_blank">Github</a> 	&#x25b0; <a href="#!">Portfolio</a> 	&#x25b0; <a href="https://www.linkedin.com/in/spencerrohan">Linkedin</a></p>
+									<p>Dev Bootcamp grad, developer with attitude - front, back, & full stack <em>JACK!</em> See the <a id="coding-skills" href="#!coding-skills" data-toggle="modal" data-target="#mdevskills" data-title="Developer Skills">coding skills</a> that occupy the brain waves. Always working towards new techniques, learning more, and living out of the comfort zone.</p>
+									<p class="text-center"><a href="/assets/docs/SpencerRohanResume.pdf">Resume</a> 	&#x25b0; <a href="https://github.com/spencerrohan" target="_blank">Github</a> 	&#x25b0; <a id="coding-skills" href="#!dev-portfolio" data-toggle="modal" data-target="#mdevportfolio" data-title="Developer Portfolio">Portfolio</a> 	&#x25b0; <a href="https://www.linkedin.com/in/spencerrohan">Linkedin</a></p>
 									<hr>
 									<div class="col-lg-12">
 										<div class="col-md-3 push-md-1">
@@ -203,7 +203,11 @@
 			</div>
 		</main>
 
+		<!-- Modals -->
 		<?php include 'includes/modalvideos.php' ?>
+		<?php include 'includes/modaldevskills.php' ?>
+		<?php include 'includes/modaldevportfolio.php' ?>
+		
 		<!-- Scripts -->
 		<script type="text/javascript" src="assets/js/app.js"></script>
 
